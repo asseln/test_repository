b06c1f6509336aff25e5c31c0ec9d50a29d41f2b
Assel <asseln86@gmail.com>
2018-08-29 15:49:28 +0100

    First commit

---
+++ Assets/MassiveNet/Examples/NetSimple/Server/Scripts/PlayerPeer.cs
@@ -0,0 +1,21 @@
+﻿using MassiveNet;
+using UnityEngine;
+
+public class PlayerPeer : MonoBehaviour {
+
+    private NetView view;
+
+	void Awake () {
+	    view = GetComponent<NetView>();
+	    view.OnReadInstantiateData += ReadInstantiateData;
+	    view.OnReadSync += ReadSync;
+	}
+
+    void ReadSync(NetStream stream) {
+        transform.position = stream.ReadVector3();
+    }
+
+    void ReadInstantiateData(NetStream stream) {
+        transform.position = stream.ReadVector3();
+    }
+}
